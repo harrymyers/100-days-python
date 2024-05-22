f256fe0159620b6adb4877682a89a3fbe3382957
Harry Myers <harrymyers257@gmail.com>
2024-05-21 21:44:51 -0700

    first test

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def is_resource_sufficient(oder_ingredients):
+    for item in oder_ingredients:
+        if oder_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+    return True
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def insert_coins():
+    print("Insert Coins")
+    quarters = int(input("Quarters"))
+    dimes = int(input("Dimes"))
+    nickels = int(input("Nickels"))
+    pennies = int(input("Pennies"))
+    total = .01 * pennies + .05 * nickels + .1 * dimes + .25 * quarters
+    return total
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def is_transaction_successful(total, cost):
+    if total < cost:
+        print("Sorry that's not enough, money refunded")
+        return False
+    print(f"change: ${round(total - cost, 2)}")
+    global profit
+    profit += cost
+    return True
+
+
+def make_coffee(name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {name}")
+
+
+is_on = True
+profit = 0
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino):")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            t = insert_coins()
+            if is_transaction_successful(t, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
